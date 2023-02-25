83fc8514f1bbf46b8d9e40f6334d67d17158c4be
eve-atieno <atienoevelyne53@gmail.com>
2023-02-25 11:21:18 +0300

    completed assignment

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,26 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
   
-end
+    def characters
+      Character.where(actor_id: id)
+    end
+  
+    def full_name
+      "#{first_name} #{last_name}"
+    end
+  
+    def shows
+      shows = []
+      characters.each do |character|
+        shows << character.show
+      end
+      shows.uniq
+    end
+
+    def list_roles
+        characters.map do |character|
+          "#{character.name} - #{character.show.name}"
+        end
+      end
+    end
